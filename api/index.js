6b30650a1ae9dcc3dc1684ae59575e04087acf71
Nico <nicolasbalbi90@gmail.com>
2023-07-19 18:00:14 -0300

    force false new db

--- api/index.js
+++ api/index.js
@@ -23,9 +23,9 @@ const { conn } = require('./src/db.js');
 const port = process.env.PORT || 3001
 
 // Syncing all the models at once.
-conn.sync({ force: true}).then(() => {
+conn.sync({ force: false}).then(() => {
   server.listen(port, async() => {
     console.log(`%s listening at ${port}`); // eslint-disable-line no-console
-    await getAllGenres()
+    // await getAllGenres()
   });
 });
